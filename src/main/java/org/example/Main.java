4415517ae50dac3a31fce5931cce7c824d9ba7c8
Isabelle Bryans <bryansi@tcd.ie>
2023-03-10 15:37:35 +0100

    Coloured shapes .ttl files, apache jena libraries not working.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.topbraid.shacl.validation.ValidationUtil;
+import org.topbraid.shacl.vocabulary.SH;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class ShaclMain {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // load data
+        Model dataGraph = SwissKnife.initAndLoadModelFromResource("movie-owl.ttl", Lang.TURTLE);
+
+        // load constraints
+        Model shapesGraph = SwissKnife.initAndLoadModelFromResource("movie-constraints.ttl", Lang.TURTLE);
+
+        // validation
+        Resource validationResult = ValidationUtil.validateModel(dataGraph, shapesGraph, false);
+
+        // write validation report
+        Model resultModel = validationResult.getModel();
+        resultModel.setNsPrefix(SH.PREFIX, SH.NS);
+        RDFDataMgr.write(new FileOutputStream("report.ttl"), resultModel, Lang.TURTLE);
     }
 }
